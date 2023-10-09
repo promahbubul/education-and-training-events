a18a64f4f30471c39b7bb1a80d39c94ca535bdcd
MAHBUBUL ALAM <33549330+promahbubul@users.noreply.github.com>
2023-10-10 00:54:08 +0600

    partner section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Services from "../../components/Services/Services";
 import { AuthContext } from "../../Providers/AuthProviders";
 import { useLoaderData } from "react-router-dom";
 import Team from "../../components/Team/Team";
+import Partner from "../../components/Partner/Partner";
 
 const Home = () => {
   const { events, team } = useLoaderData();
@@ -21,6 +22,9 @@ const Home = () => {
           ))}
         </div>
         <div className="my-10 bg-slate-300 py-10 ">
+          <h2 className="border-b-4 border-purple-600 w-4/12 mx-auto text-center mb-10 py-5 text-4xl font-extrabold">
+            Our Team
+          </h2>
           <div
             className="grid md:grid-cols-4 grid-cols-2 max-w-6xl justify-between
          mx-auto  gap-5"
@@ -29,6 +33,13 @@ const Home = () => {
               <Team person={person}></Team>
             ))}
           </div>
+        </div>
+
+        <div className="max-w-6xl mt-10  mx-auto">
+          <h2 className="border-b-4 border-purple-600 w-4/12 mx-auto text-center mb-10 py-5 text-4xl font-extrabold">
+            Our Partner
+          </h2>
+          <Partner></Partner>
         </div>
       </div>
     </div>
---
+++ src/components/Partner/Partner.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Partner = () => {
+  return (
+    <div className="grid md:grid-cols-4 grid-cols-2 gap-5  py-10 w-full flex-row justify-between items-center ">
+      <img
+        className="w-[100px] h-[50px]"
+        src="https://i.ibb.co/XLH3bj7/296d05a1-c52a-4f5e-abf2-0d49d4c0d6b3.png"
+        alt=""
+      />
+      <img
+        className="w-[100px] h-[50px]  "
+        src="https://i.ibb.co/3Fb7rLT/Amazon-logo-svg.png"
+        alt=""
+      />
+      <img
+        className="w-[100px] h-[50px]"
+        src="https://i.ibb.co/FXNQ0w5/image-173-png.webp"
+        alt=""
+      />
+      <img
+        className="w-[100px] h-[50px]"
+        src="https://i.ibb.co/7zHs97Y/Toyota-Logo-1989.png"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Partner;
